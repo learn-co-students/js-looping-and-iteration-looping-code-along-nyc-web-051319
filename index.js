3aeb6c72a46e3c695e595c40a9b4894d8aa8a538
locky5 <daikevin19@yahoo.com>
2019-06-24 02:31:45 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(names, holiday) {
+  let cards = []
+  for (let i = 0; i < names.length; i++) {
+    cards.push(`Thank you, ${names[i]}, for the wonderful ${holiday} gift!`);
+  }
+  return cards;
+}
+
+function countdown(number) {
+  while (number >= 0) {
+    console.log(number);
+    number--;
+  }
+}
