c3d0ba3ddbb5e9cee8184e5a654fcbf612ffe2c4
marycriv <maryecrivelli@gmail.com>
2019-06-24 16:29:38 -0400

    Done

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names, adj) {
+  let arry = []
+  for (let i = 0; i < names.length; i++) {
+    arry.push(`Thank you, ${names[i]}, for the wonderful ${adj} gift!`);
+  }
+  return arry;
+}
+
+function countdown(num) {
+  while (num >= 0) {
+    console.log(num);
+    num--;
+  }
+}
