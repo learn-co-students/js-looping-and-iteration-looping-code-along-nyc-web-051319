2c53a1b1157bc5df5a8435ae6928ac172ab8ebc5
Amy Cen <cenamy@gmail.com>
2019-06-24 09:42:56 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solutions in this file
+
+function writeCards(names, event) {
+    for (i = 0, i < names.length, i++){
+        console.log(`Thank you, ${names[i]}, for the wonderful ${event} gift.`)
+    }
+}
+
+function countdown(number) {
+    while (number >= 0) {
+        console.log(number--);
+    }
+}
