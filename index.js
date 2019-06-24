ca58e6a5885a7fa6f7401c845101acca33d67ccb
Amy Cen <amy.cen@blackrock.com>
2019-06-24 13:50:53 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 // Code your solutions in this file
 
 function writeCards(names, event) {
-    for (i = 0, i < names.length, i++){
-        console.log(`Thank you, ${names[i]}, for the wonderful ${event} gift.`)
+  let messages = [];
+    for (let i = 0; i < names.length; i++){
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
     }
+    return messages;
 }
+
 
 function countdown(number) {
     while (number >= 0) {
