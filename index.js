817ce47938251cdffda1c08b21f1782431bdf94a
Christian Pinto <pintochristian2@gmail.com>
2019-06-25 13:41:23 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards( name, event) {
+  let arr= []
+  for( let i = 0; i < name.length; i++){
+    arr.push('Thank you, ' + name[i] + ', for the wonderful ' + event + ' gift!')
+  }
+  return arr
+}
+
+function countdown(number){
+  while(number> 0){
+    console.log(number);
+    number -= 1;
+  }
+  console.log(number);
+}
